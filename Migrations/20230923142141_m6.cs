024325b9c0082dd0175dc72899ff2bf1f65822dd
John Doe <john@example.com>
2023-10-29 21:58:39 +0530

    my commit

---
+++ Migrations/20230923142141_m6.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelLove.Migrations
+{
+    public partial class m6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Bus_BusDetailsBusId",
+                table: "Transactions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Bus",
+                table: "Bus");
+
+            migrationBuilder.RenameTable(
+                name: "Bus",
+                newName: "BusDetails");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BusDetails",
+                table: "BusDetails",
+                column: "BusId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_BusDetails_BusDetailsBusId",
+                table: "Transactions",
+                column: "BusDetailsBusId",
+                principalTable: "BusDetails",
+                principalColumn: "BusId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_BusDetails_BusDetailsBusId",
+                table: "Transactions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BusDetails",
+                table: "BusDetails");
+
+            migrationBuilder.RenameTable(
+                name: "BusDetails",
+                newName: "Bus");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Bus",
+                table: "Bus",
+                column: "BusId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Bus_BusDetailsBusId",
+                table: "Transactions",
+                column: "BusDetailsBusId",
+                principalTable: "Bus",
+                principalColumn: "BusId");
+        }
+    }
+}
---
+++ Migrations/20230923161919_m65.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelLove.Migrations
+{
+    public partial class m65 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SleeperSeats",
+                table: "bookings",
+                newName: "Sleeper");
+
+            migrationBuilder.RenameColumn(
+                name: "SecondACSeats",
+                table: "bookings",
+                newName: "SecondAC");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstACSeats",
+                table: "bookings",
+                newName: "FirstAC");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Sleeper",
+                table: "bookings",
+                newName: "SleeperSeats");
+
+            migrationBuilder.RenameColumn(
+                name: "SecondAC",
+                table: "bookings",
+                newName: "SecondACSeats");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstAC",
+                table: "bookings",
+                newName: "FirstACSeats");
+        }
+    }
+}
